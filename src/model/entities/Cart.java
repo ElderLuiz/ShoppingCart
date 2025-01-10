f58d2b7722c6295e102f1dfc3643cd72a31f0be0
Elder Luiz <elderluizmoreira1@gmail.com>
2025-01-10 16:48:28 -0300

    feat/Cart and Product

---
+++ src/model/entities/Cart.java
@@ -0,0 +1,83 @@
+package model.entities;
+
+public class Cart {
+
+    private int id;
+    private String name;
+    private String category;
+    private double price;
+    private int quantity;
+    private double totalValue;
+
+
+    public Cart() {
+    }
+
+
+    public Cart(int id, String name, String category, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+        this.totalValue = price * quantity;
+    }
+
+   
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+        calculateTotalValue();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        calculateTotalValue();
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+    private void calculateTotalValue() {
+        this.totalValue = this.price * this.quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart ID: " + id + ", Name: " + name + ", Category: " + category +
+               ", Product Price: " + price + ", Quantity: " + quantity +
+               ", Total Value: " + totalValue + "]";
+    }
+}
